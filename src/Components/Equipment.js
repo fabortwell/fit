b01d33f7d0818c24066b20465753c6d1ea222330
Brian Sitati <briansit93@gmail.com>
2022-05-26 11:04:14 +0300

    added testimonial component

--- src/Components/Equipment.js
+++ src/Components/Equipment.js
@@ -6,14 +6,14 @@ const Equipment = () => {
     <div>Equipment</div>
     <div class="grid grid-cols-3 gap-4 flex items-center">
   <div class="mb-4">
-    <img src="https://mdbootstrap.com/img/new/standard/city/047.jpg" class="max-w-full h-auto rounded-lg" alt=""/>
+    <img src="https://images.contentstack.io/v3/assets/blt1d89a78b502b83f3/bltfcd8733ca2371582/61143decc44c171460d4d54e/Listing_Cables.png" class="max-w-full h-auto rounded-lg" alt=""/>
   </div>
 
   <div class="mb-4">
-    <img src="https://mdbootstrap.com//img/Photos/Square/1.jpg" class="max-w-full h-auto rounded-full" alt=""/>
+    <img src="https://images.contentstack.io/v3/assets/blt1d89a78b502b83f3/blt7671d065ea9f3415/61143dec067c9314b57e56d4/Listing_Treadmills.png" class="max-w-full h-auto rounded-full" alt=""/>
   </div>
   <div class="mb-4">
-    <img src="https://mdbootstrap.com/img/new/standard/city/044.jpg" class="max-w-full h-auto rounded-full" alt=""/>
+    <img src="https://images.contentstack.io/v3/assets/blt1d89a78b502b83f3/blt2fb538dd29425107/6148f172d7fc263e226a7a18/Listing_Cycling.png" class="max-w-full h-auto rounded-full" alt=""/>
   </div>
 </div>
 </>
