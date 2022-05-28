166c1e232198f4efdfb028d91afac94de135965c
Brian Sitati <briansit93@gmail.com>
2022-05-29 00:48:08 +0300

    defined the loadMap function ()

--- src/Components/Map.js
+++ src/Components/Map.js
@@ -44,6 +44,37 @@ export class CurrentLocation extends React.Component {
         }
       }
 
+      loadMap() {
+        if (this.props && this.props.google) {
+          // checks if google is available
+          const { google } = this.props;
+          const maps = google.maps;
+    
+          const mapRef = this.refs.map;
+    
+          // reference to the actual DOM element
+          const node = ReactDOM.findDOMNode(mapRef);
+    
+          let { zoom } = this.props;
+          const { lat, lng } = this.state.currentLocation;
+          const center = new maps.LatLng(lat, lng);
+    
+          const mapConfig = Object.assign(
+            {},
+            {
+              center: center,
+              zoom: zoom
+            }
+          );
+    
+          // maps.Map() is constructor that instantiates the map
+          this.map = new maps.Map(node, mapConfig);
+        }
+      }
+
+
+
+
       componentDidMount() {
         if (this.props.centerAroundCurrentLocation) {
           if (navigator && navigator.geolocation) {
@@ -61,7 +92,7 @@ export class CurrentLocation extends React.Component {
         this.loadMap();
       }
 
-      
+
 CurrentLocation.defaultProps = {
     zoom: 14,
     initialCenter: {
