52156ff01af3358d461593aa4ad67659e75f2b50
Brian Sitati <briansit93@gmail.com>
2022-06-01 13:31:46 +0300

    fixed a bug in hero section

--- src/Components/Hero.js
+++ src/Components/Hero.js
@@ -5,7 +5,10 @@ const Hero = () => {
   return (
  <div>
     <div className='hero'> 
-    <img src="https://cdn.shopify.com/s/files/1/0078/6006/0207/files/iFitNut-Web_Hero-Desktop_1.jpg?v=1609781276" alt="fit" className="hero-img"/>
+    <img src="https://cdn.shopify.com/s/files/1/0078/6006/0207/files/iFitNut-Web_Hero-Desktop_1.jpg?v=1609781276" alt="fit" className="hero-img"
+    
+    
+    />
    </div> 
     </div>
   )
